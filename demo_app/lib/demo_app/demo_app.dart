7b08154dbe6e9c9da85ee005c389064b09105171
ntaoo <ntaoo.g@gmail.com>
2016-06-02 07:48:44 +0900

    First demo app commit with only MdButton.

---
+++ demo_app/lib/demo_app/demo_app.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'package:material2_dart/components/button/button.dart';
+
+@Component(
+    selector: 'demo-app',
+    templateUrl: 'demo_app.html',
+    styleUrls: const ['demo_app.scss.css'],
+    directives: const [MdButton])
+class DemoApp {}
---
+++ demo_app/web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/platform/browser.dart';
+
+import 'package:demo_app/demo_app/demo_app.dart';
+
+main() {
+  bootstrap(DemoApp);
+}
