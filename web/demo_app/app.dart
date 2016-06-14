7064b59a7c4e238b50b782e927df4d1f3e84142c
Adao Jr <itsjunnior@gmail.com>
2016-06-14 11:22:50 -0300

    remove name

--- web/demo_app/app.dart
+++ web/demo_app/app.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, Adao Jr.. All rights reserved. Use of this source code
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
