e66a211a113486616973cc149a7d56801ea36deb
ntaoo <ntaoo.g@gmail.com>
2016-06-19 12:19:36 +0900

    Port icon demo #11.

--- web/demo_app/demo_app/demo_app.dart
+++ web/demo_app/demo_app/demo_app.dart
@@ -11,8 +11,9 @@ import 'package:material2_dart/components/icon/icon_registry.dart';
 import 'package:material2_dart/components/list/list.dart';
 import 'package:material2_dart/components/toolbar/toolbar.dart';
 
-import '../card/card_demo.dart';
 import '../button/button_demo.dart';
+import '../card/card_demo.dart';
+import '../icon/icon_demo.dart';
 
 @Component(
     selector: 'home',
@@ -44,7 +45,8 @@ class Home {}
 @RouteConfig(const [
   const Route(path: '/', name: 'Home', component: Home, useAsDefault: true),
   const Route(path: '/button', name: 'Button', component: ButtonDemo),
-  const Route(path: '/card', name: 'Card', component: CardDemo)
+  const Route(path: '/card', name: 'Card', component: CardDemo),
+  const Route(path: '/icon', name: 'Icon', component: IconDemo)
 ])
 class DemoApp {
   Router router;
