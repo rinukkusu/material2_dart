6917a341e966d07693b878976a26b32d3ee48543
ntaoo <ntaoo.g@gmail.com>
2016-06-21 11:02:56 +0900

    Fix test package's complaint.(No top-level main() function defined).

--- test/components/checkbox/checkbox_test.dart
+++ test/components/checkbox/checkbox_test.dart
@@ -1,1 +1,24 @@
-/// TODO: port this test later.
+import 'package:angular2/core.dart';
+
+//import 'package:angular2/platform/browser.dart';
+import 'package:angular2_testing/angular2_testing.dart';
+@TestOn('browser')
+import 'package:test/test.dart';
+
+void main() {
+//  TestComponentBuilder builder;
+
+  initAngularTests();
+
+  setUpProviders(() {
+    return const [
+      const Provider(TestComponentBuilder, useClass: TestComponentBuilder)
+    ];
+  });
+
+//  ngSetUp((TestComponentBuilder tcb) {
+//    builder = tcb;
+//  });
+
+  ngTest('checkbox test', () async {}, skip: 'Implement later.');
+}
