29dd345c3fdee1f6b1261827677a7dd5f14f36e3
ntaoo <ntaoo.g@gmail.com>
2016-06-23 09:53:22 +0900

    Port slide_toggle demo.

---
+++ web/demo_app/slide_toggle/slide_toggle_demo.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/core.dart';
+import 'package:material2_dart/components/slide_toggle/slide_toggle.dart';
+
+@Component(
+    selector: 'switch-demo',
+    templateUrl: 'slide_toggle_demo.html',
+    styleUrls: const ['slide_toggle_demo.css'],
+    directives: const [MdSlideToggle])
+class SlideToggleDemo {
+  bool firstToggle = false;
+}
