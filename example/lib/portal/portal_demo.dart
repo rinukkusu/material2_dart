a76e15b6466d4e5ebe02b95c7b1587b90f48351d
ntaoo <ntaoo.g@gmail.com>
2016-06-25 16:03:28 +0900

    Add Portal demo.

---
+++ example/lib/portal/portal_demo.dart
@@ -0,0 +1,24 @@
+import "package:angular2/core.dart";
+import "package:material2_dart/core/core.dart";
+
+@Component(
+    selector: "portal-demo",
+    templateUrl: "portal_demo.html",
+    styleUrls: const ["portal_demo.scss.css"],
+    directives: const [TemplatePortalDirective, PortalHostDirective])
+class PortalDemo {
+  @ViewChildren(TemplatePortalDirective)
+  QueryList<Portal<dynamic>> templatePortals;
+  Portal<dynamic> selectedPortal;
+
+  Portal get programmingJoke => templatePortals.first;
+
+  Portal get mathJoke => templatePortals.last;
+
+  ComponentPortal get scienceJoke => new ComponentPortal(ScienceJoke);
+}
+
+@Component(
+    selector: "science-joke",
+    template: '''<p> 100 kilopascals go into a bar. </p>''')
+class ScienceJoke {}
